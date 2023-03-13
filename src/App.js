cb4f86cc2fbe016565cccccb49fae3f191e2ef18
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-13 15:29:13 +0200

    Initial create.

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return <div>Testing App component...</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
