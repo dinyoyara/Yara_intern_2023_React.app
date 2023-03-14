ca61aa71bb9f7ed825e63730bd82f81a8349d605
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-14 11:26:48 +0200

    Update Header component.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Row } from 'react-flexa';
 
 import logo from '../../assets/images/logo.jpg';
+import StyledBanner from './Banner/styledBanner';
+import StyledImg from '../../shared/Image/styledImage';
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt='logo' />
-            <div>Header</div>
-        </div>
+        <Row style={{ margin: '0' }}>
+            <StyledImg src={logo} alt='Logo' width='80px' height='80px' />
+            <StyledBanner />
+        </Row>
     );
 };
 
