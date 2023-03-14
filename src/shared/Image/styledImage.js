f21d316aa5d3f12482105ea45f6c132f7a71b105
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-14 15:30:30 +0200

    FIx image cursor prop.

--- src/shared/Image/styledImage.js
+++ src/shared/Image/styledImage.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const StyledImage = styled('img')`
     height: ${(p) => p.height};
     width: ${(p) => p.width};
+    cursor: pointer;
 `;
 
 export default StyledImage;
