abd0a0b931a360fd43a35e2f845748741a116cff
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-14 17:17:14 +0200

    Add basic Form and Table components.

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Row, Col } from 'react-flexa';
+import { Switch, Route } from 'react-router-dom';
 
-import StyledSide from './Side/styledSide';
+import StyledSidebar from './Sidebar/styledSidebar';
+import Table from './Table';
+import Form from './Form';
 import { logoSize } from '../../styles/const';
 
 const Main = () => {
     return (
         <Row gutter='0px'>
             <Col xs={`${logoSize}px`} gutter='0px'>
-                <StyledSide />
+                <StyledSidebar />
             </Col>
             <Col xs={`calc(100vw - ${logoSize}px)`} gutter='0px'>
-                <div>Main</div>
+                <Switch>
+                    <Route exact path='/form' component={Form} />
+                    <Route exact path='/table' component={Table} />
+                </Switch>
             </Col>
         </Row>
     );
