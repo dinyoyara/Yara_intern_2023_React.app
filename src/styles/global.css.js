1dc58218b869c6c3b3ef58a511b7a44d535a785e
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-14 11:34:07 +0200

    Update GlobalStyles.

--- src/styles/global.css.js
+++ src/styles/global.css.js
@@ -1,11 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    body {
-        margin: 0;
-        padding: 0;
+    html {
         font-family: Arial, Helvetica, Sans-Serif;
         font-size: 16px;
+    };
+    body {
+        box-sizing: border-box;
+    };
+    * {
+        margin: 0;
+        padding: 0;
     }
     `;
 
