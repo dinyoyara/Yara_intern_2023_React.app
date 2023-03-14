ed60f89e619ef80c83e6e2505c0df9e9efe3bcef
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-14 12:02:02 +0200

    Add routing in app.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import GlobalStyle from './styles/global.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
     return (
         <BrowserRouter>
             <GlobalStyle />
             <Header />
-            <Main />
+            <Switch>
+                <Route exact path='/not-found' component={ErrorPage} />
+                <Route path='/' component={Main} />
+            </Switch>
         </BrowserRouter>
     );
 }
