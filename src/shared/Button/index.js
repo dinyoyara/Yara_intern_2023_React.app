487ba86324da919f2f97eb950f1741467f7f8749
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-15 17:08:16 +0200

    Implement Button component.

---
+++ src/shared/Button/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import StyledButton from './styledButton';
+
+const Button = ({ text }) => {
+    return <StyledButton>{text}</StyledButton>;
+};
+
+export default Button;
