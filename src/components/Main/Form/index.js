15cdb381ba43d8dbb273f8f31297d08587de2198
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-15 17:11:11 +0200

    Add Form content and styles.

--- src/components/Main/Form/index.js
+++ src/components/Main/Form/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Col, Row } from 'react-flexa';
+
+import Input from './Input';
+import Button from '../../../shared/Button';
+import StyledForm from './styledForm';
+import { inputSize } from '../../../styles/const';
 
 const Form = () => {
-    return <div>Form</div>;
+    return (
+        <StyledForm>
+            <Input
+                type='text'
+                id='firstName'
+                label='First name'
+                placeholder='Enter your first name here'
+                width={`${inputSize}px`}
+            />
+            <Input
+                type='text'
+                id='LastName'
+                label='Last name'
+                placeholder='Enter your last name here'
+                width={`${inputSize}px`}
+            />
+
+            <Input type='text' id='Age' label='Age' placeholder='Enter your age here' width={`${inputSize}px`} />
+
+            <Row gutter='0px' justifyContent='space-between'>
+                <Col xs={4} gutter='0px'>
+                    <Button text='Clear' />
+                </Col>
+                <Col xs={4} gutter='0px'>
+                    <Button text='Sumbit' />
+                </Col>
+            </Row>
+        </StyledForm>
+    );
 };
 
 export default Form;
---
+++ src/components/Main/Form/styledForm.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledForm = styled('form')`
+    width: 200px;
+    margin: 20px 50px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+export default StyledForm;
