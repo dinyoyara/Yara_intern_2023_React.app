cb0d51bde3cf649a3250a159c0d60ac657eb353a
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-17 09:53:39 +0200

    Update Error page.

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Row, Col } from 'react-flexa';
 
 const ErrorPage = () => {
-    return <div>Not Found</div>;
+    return (
+        <Row gutter='0px' justifyContent='center' style={{ paddingTop: '80px' }}>
+            <Col style={{ fontSize: '32px', color: 'red' }}>NOT FOUND</Col>
+        </Row>
+    );
 };
 
 export default ErrorPage;
